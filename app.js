1d74ae769553eea7d8b00c85c3b7337814b2545a
Noema <noemazhao@gmail.com>
2023-03-30 21:07:00 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// This is where you can define the functionality of your app
+
+// Define a function to display the map
+function displayMap() {
+  // TODO: Add code to display the map here
+}
+
+// Define a function to generate the audio tour
+function generateAudioTour() {
+  // TODO: Add code to generate the audio tour here
+}
+
+// Define a function to upload user comments
+function uploadComment() {
+  // TODO: Add code to upload user comments here
+}
+
+// Attach event listeners to your app elements
+document.querySelector('#map-button').addEventListener('click', displayMap);
+document.querySelector('#audio-tour-button').addEventListener('click', generateAudioTour);
+document.querySelector('#comment-button').addEventListener('click', uploadComment);
+
---
+++ index.js
@@ -0,0 +1,5 @@
+// This is the main entry point for your app
+// You can use it to initialize your app, and add event listeners
+
+console.log('Hello, world!');
+
